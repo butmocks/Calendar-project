d644b4e857cda51efaeb4cb67b1ea84306c545fd
butmocks <butmocks@gmail.com>
2022-08-25 00:06:33 +0300

    34

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -11,7 +11,9 @@ const Navigation = ({ weekDates }) => {
     <header className="calendar__header">
       {weekDates.map(dayDate => (
         <div className="calendar__day-label day-label">
-          <span className="day-label__day-name">{days[dayDate.getDay()]}</span>
+          <span className="day-label__day-name">
+            {days[dayDate.getDay()]}
+            </span>
           <span className="day-label__day-number">{dayDate.getDate()}</span>
         </div>
       ))}
